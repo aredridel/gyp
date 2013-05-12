9bc0cc5bdd526328cabfb12ec5efd4a0a456df2c
Aria Stewart <aredridel@nbtsc.org>
2013-05-12 00:25:32 -0400

    Handle variables and select strings for expansion

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var merge = require('gyp-merge');
 var path = require('path');
 var fs = require('fs');
-
+var expand = require('gyp-expansions');
 var loader = require('gyp-load');
 
-var gyp = module.exports = function gyp(arg, relative) {
+var gyp = module.exports = function gyp(arg, variables) {
     if (typeof arg == 'string' || arg instanceof String) {
         arg = loader(arg);
     }
@@ -13,7 +13,7 @@ var gyp = module.exports = function gyp(arg, relative) {
         throw new Error("Root must be an object");
     }
 
-    /// @todo do pre-phase expansions here
+    arg = applyExpansions(arg, variables || {}, 'pre');
 
     /// @todo do conditional processing
 
@@ -21,3 +21,49 @@ var gyp = module.exports = function gyp(arg, relative) {
 
     return arg;
 };
+
+function applyExpansions(arg, variables, which) {
+    var m;
+    if (arg.variables) {
+        variables = Object.create(variables);
+        for (var v in arg.variables) {
+            if (m = /(.*)%$/.exec(v)) {
+                if (!variables[m[1]]) {
+                    variables[m[1]] = arg.variables[v];
+                }
+            } else {
+                variables[v] = arg.variables[v];
+            }
+        }
+    }
+
+    arg = recurse(arg, function (o) {
+        return applyExpansions(o, variables, which);
+    }, function (s) {
+        if (typeof s == 'string' || s instanceof String) {
+            return expand(s, variables, which);
+        } else {
+            return s;
+        }
+    });
+
+    return arg;
+}
+
+function recurse(arg, onObject, onScalar) {
+    var out = {};
+    for (var v in arg) {
+        if (Array.isArray(arg[v])) {
+            out[v] = arg[v].map(function (e) {
+                return recurse(e, onObject, onScalar);
+            });
+        } else if (typeof arg[v] == 'object') {
+            var t = onObject(arg[v]);
+            out[v] = recurse(t, onObject, onScalar);
+        } else {
+            out[v] = onScalar(arg[v]);
+        }
+    }
+
+    return out;
+}
