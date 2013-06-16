eaed14437d9c207ab34cca2a3c33d25a6d88477b
Aria Stewart <aredridel@nbtsc.org>
2013-06-15 23:38:16 -0400

    Shaping up
    - Integrated gyp-expansions
    - switched to async interface entirely

--- index.js
+++ index.js
@@ -1,69 +1,86 @@
 var merge = require('gyp-merge');
 var path = require('path');
 var fs = require('fs');
-var expand = require('gyp-expansions');
-var loader = require('gyp-load');
+var expansions = require('gyp-expansions');
+var load = require('gyp-load');
 
-var gyp = module.exports = function gyp(arg, variables) {
+var gyp = module.exports = function gyp(arg, variables, cb) {
     if (typeof arg == 'string' || arg instanceof String) {
-        arg = loader(arg);
+        arg = load(arg);
     }
 
     if (typeof arg != 'object') {
         throw new Error("Root must be an object");
     }
 
-    arg = applyExpansions(arg, variables || {}, 'pre');
+    recurse(arg, variables, cb);
+
+    return;
 
     /// @todo do conditional processing
 
     /// @todo do post-phase expansions
 
-    return arg;
+    cb(null, arg);
 };
 
-function applyExpansions(arg, variables, which) {
-    var m;
-    if (arg.variables) {
-        variables = Object.create(variables);
-        for (var v in arg.variables) {
-            if (m = /(.*)%$/.exec(v)) {
-                if (!variables[m[1]]) {
-                    variables[m[1]] = arg.variables[v];
-                }
-            } else {
-                variables[v] = arg.variables[v];
-            }
-        }
-    }
-
-    arg = recurse(arg, function (o) {
-        return applyExpansions(o, variables, which);
-    }, function (s) {
-        if (typeof s == 'string' || s instanceof String) {
-            return expand(s, variables, which);
-        } else {
-            return s;
-        }
-    });
-
-    return arg;
+function recurse(obj, variables, cb) {
+    oiter(obj, function(e, c) {
+        handle(e, variables, c);
+    }, cb);
 }
 
-function recurse(arg, onObject, onScalar) {
-    var out = {};
-    for (var v in arg) {
-        if (Array.isArray(arg[v])) {
-            out[v] = arg[v].map(function (e) {
-                return recurse(e, onObject, onScalar);
+function oiter(obj, proc, cb) {
+    var done = 0;
+    var result = {};
+    var length = Object.keys(obj).length;
+    for (var i in obj) {
+        (function(j) {
+            setImmediate(function() {
+                proc(obj[j], function(err, res) {
+                    if (err) cb(err);
+                    result[j] = res;
+                    if (++done == length) cb(null, result);
+                });
             });
-        } else if (typeof arg[v] == 'object') {
-            var t = onObject(arg[v]);
-            out[v] = recurse(t, onObject, onScalar);
-        } else {
-            out[v] = onScalar(arg[v]);
-        }
+        })(i);
     }
+}
 
-    return out;
+function handle(thing, variables, cb) {
+    if (Array.isArray(thing)) {
+        expansions.expandArray(thing, variables, 'pre', function (err, res) {
+            cb(null, (typeof res != 'undefined' ? res : thing).map(function (e) {
+                return recurse(e, variables);
+            }));
+        });
+    } else if (typeof thing == 'object') {
+        var m;
+        var out = {};
+        if (thing.variables) {
+            variables = Object.create(variables);
+            for (var v in thing.variables) {
+                if (m = /(.*)%$/.exec(v) && !variables[m[1]]) {
+                    variables[m[1]] = thing.variables[v];
+                } else {
+                    variables[v] = thing.variables[v];
+                }
+            }
+        }
+
+        if (thing.conditions) {
+        }
+
+        for (var v in thing) {
+            if (v != 'variables') continue;
+            out[v] = recurse(thing[v], variables);
+        }
+
+        cb(null, out);
+    } else {
+        thing = "" + thing;
+        expansions.expandString(thing, variables, 'pre', function(err, res) {
+            cb(null, typeof res != 'undefined' ? res : thing);
+        });
+    }
 }
--- test/loading.js
+++ test/loading.js
@@ -2,8 +2,9 @@ var test = require('tap').test;
 var gyp = require('..');
 
 test("parse simple dictionary", function(t) {
-    var out = gyp({ a: 1, b: "test" });
-    t.equal(out.a, 1, "a should be 1");
-    t.equal(out.b, "test", "b should 'test'");
-    t.end();
+    gyp({ a: 1, b: "test" }, {}, function(err, out) {
+        t.equal(out.a, "1", "a should be 1");
+        t.equal(out.b, "test", "b should 'test'");
+        t.end();
+    });
 });
