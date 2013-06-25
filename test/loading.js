44f55575e8a055b42388440a4e29f9ea18e4284c
Aria Stewart <aredridel@nbtsc.org>
2013-06-24 23:18:31 -0400

    Add a file-loading test case and expand the README

--- test/loading.js
+++ test/loading.js
@@ -8,3 +8,11 @@ test("parse simple dictionary", function(t) {
         t.end();
     });
 });
+
+test("parse from file", function(t) {
+    gyp(__dirname + "/test.gyp", {}, function(err, out) {
+        t.equal(out.a, "1", "a should be 1");
+        t.equal(out.b, "test", "b should 'test'");
+        t.end();
+    });
+});
